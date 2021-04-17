ff22e697f3ac0ded2be9bc5e62a56e33bba3f8b2
LeptoFlare <leptoflare@lepto.tech>
2021-04-17 19:15:42 -0400

    Rename template.tsx to render.tsx

--- src/dashboard/scene-selector.tsx
+++ src/dashboard/scene-selector.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { css } from "@emotion/core";
 import { TextField, Button, Box } from "@material-ui/core";
 
-import render from "./utils/template";
+import render from "./utils/render";
 import useReplicant from "./utils/useReplicant";
 import Section from "./utils/Section";
 
