1ac46639b213f365eb08caac403927809e4d37da
LeptoFlare <leptoflare@lepto.tech>
2021-04-19 01:53:27 -0400

    Add optional label to Section

--- src/Section.tsx
+++ src/Section.tsx
@@ -1,10 +1,23 @@
 import * as React from "react"
-import { Box } from "@material-ui/core"
+import { css } from "@emotion/css"
+import { Box, InputLabel } from "@material-ui/core"
+import { theme } from "./render"
 
-const Section = ({ children }) => (
-  <Box bgcolor="grey.300" p={1.5} mb={1.5}>
-    {children}
-  </Box>
+const Section = ({ title = null, children }) => (
+  <>
+    {title && (
+      <InputLabel
+        className={css`
+          margin-bottom: ${theme.spacing(0.5)}px;
+        `}
+      >
+        {title}
+      </InputLabel>
+    )}
+    <Box bgcolor="grey.300" p={1.5} mb={1.5}>
+      {children}
+    </Box>
+  </>
 )
 
 export default Section
