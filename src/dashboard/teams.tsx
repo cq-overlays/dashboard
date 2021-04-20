6b53cd86fbc301911bddaa4e691385f027ebd5f1
LeptoFlare <leptoflare@lepto.tech>
2021-04-20 00:49:55 -0400

    Hydrate components with state management! Add dispatch and replicant handlers to all applicable components

--- src/dashboard/teams.tsx
+++ src/dashboard/teams.tsx
@@ -12,6 +12,9 @@ import Section from "../components/Section"
 import useTeamsReplicant from "../hooks/useTeamsReplicant"
 
 const Panel = () => {
+  const { teams, replicant, dispatch, replicate } = useTeamsReplicant()
+  console.log(teams, replicant)
+
   return (
     <Box>
       <Section
@@ -30,45 +33,53 @@ const Panel = () => {
   )
 }
 
-const Scoreboard = ({ teams, dispatch, replicate }) => (
-  <>
-    <ScoreHalf
-      type="scoreA"
-      score={teams.scoreA}
-          <Score score={scoreA} setScore={setScoreA} left={true} />
-          <Box>
-            <Button
-              onClick={() => {
-                setScoreA(0)
-                setScoreB(0)
-              }}
-              className={css`
-                font-size: ${theme.spacing(2)}px;
-                min-width: ${theme.spacing(3.5)}px;
-                min-height: ${theme.spacing(3.5)}px;
-              `}
-            >
-              -
-            </Button>
-    />
-    <Button
-      className={css`
-        font-size: ${theme.spacing(2)}px;
-        min-width: ${theme.spacing(3.5)}px;
-        min-height: ${theme.spacing(3.5)}px;
-      `}
-    >
-      -
-    </Button>
-    <ScoreHalf
-      type="scoreB"
-      score={teams.scoreB}
-      replicate={replicate}
-      reversed={true}
-    />
-  </>
-)
+const Scoreboard = ({ teams, dispatch, replicate }) => {
+  const dispatchScoreResetter = () => dispatch({ type: "resetScores" })
 
+  return (
+    <>
+      <ScoreHalf
+        {...{
+          type: "A",
+          score: teams.scoreA,
+          color: teams.colors[0],
+          dispatch,
+          replicate,
+        }}
+      />
+      <Button
+        onClick={dispatchScoreResetter}
+        className={css`
+          font-size: ${theme.spacing(2)}px;
+          min-width: ${theme.spacing(3.5)}px;
+          min-height: ${theme.spacing(3.5)}px;
+        `}
+      >
+        -
+      </Button>
+      <ScoreHalf
+        {...{
+          type: "B",
+          score: teams.scoreB,
+          color: teams.colors[1],
+          dispatch,
+          replicate,
+        }}
+        reversed={true}
+      />
+    </>
+  )
+}
+
+const ScoreHalf = ({
+  type,
+  score,
+  color,
+  replicate,
+  dispatch,
+  reversed = false,
+}) => {
+  const dispatchScore = score => dispatch({ type: `setScore${type}`, score })
 const Nameboard = ({ teams, dispatch, replicate, replicant }) => {
           </Box>
           <Score score={scoreB} setScore={setScoreB} left={false} />
@@ -154,17 +165,32 @@ const Nameboard = ({ teams, dispatch, replicate, replicant }) => {
     </>
   )
 }
+  //   function replicateScore() {
+  //     switch (type) {
+  //       case "A":
+  //         return replicate([{ score }, {}])
+  //       case "B":
+  //         return replicate([{}, { score }])
+  //       default:
+  //         throw new Error(
+  //           `Unsupported team type '${type}' for ScoreHalf effect.`
+  //         )
+  //     }
+  //   },
+  //   [score]
+  // )
 
-const ScoreHalf = ({ type, score, replicate, handler, reversed = false }) => {
   type ButtonProps = {
     key: string
     color: "primary" | "secondary"
     icon: any
     mut: any
   }
+
   const fragments = [
+    <InkPreview color={color} />,
     <Box
-      key={`${type}-buttons`}
+      key={`score${type}-buttons`}
       className={css`
         display: flex;
         flex-direction: column;
@@ -172,20 +198,16 @@ const ScoreHalf = ({ type, score, replicate, handler, reversed = false }) => {
     >
       {[
         {
-          key: `${type}-inc`,
+          key: `score${type}-inc`,
           color: "primary",
           icon: <AddRounded />,
-          mut: () => {
-            handler(score + 1)
-          },
+          mut: s => s + 1,
         },
         {
-          key: `${type}-dec`,
+          key: `score${type}-dec`,
           color: "secondary",
           icon: <RemoveRounded />,
-          mut: () => {
-            handler(score - 1)
-          },
+          mut: s => s - 1,
         },
       ].map((props: ButtonProps) => (
         <Box my={0.5} key={props.key}>
@@ -198,14 +220,14 @@ const ScoreHalf = ({ type, score, replicate, handler, reversed = false }) => {
             `}
             variant="contained"
             color={props.color}
-            onClick={props.mut}
+            onClick={() => dispatchScore(props.mut(score))}
           >
             {props.icon}
           </Button>
         </Box>
       ))}
     </Box>,
-    <Box p={1.5} key={`${type}-input`}>
+    <Box p={1.5} key={`score${type}-input`}>
       <TextField
         className={css`
           input {
@@ -219,18 +241,82 @@ const ScoreHalf = ({ type, score, replicate, handler, reversed = false }) => {
       />
     </Box>,
   ]
-  if (reversed) {
-    fragments.reverse()
-  }
+  reversed && fragments.reverse()
   return <>{fragments.map(f => f)}</>
 }
 
+const Nameboard = ({ teams, dispatch, replicate, replicant }) => {
+  const replicateNameboard = () =>
+    replicate([
+      { name: teams.nameA, color: teams.colors[0] },
+      { name: teams.nameB, color: teams.colors[1] },
+    ])
+
+  const isUpdated = () =>
+    replicant?.[0]?.name === teams.nameA &&
+    replicant?.[1]?.name === teams.nameB &&
+    replicant?.[0]?.color === teams.colors[0] &&
+    replicant?.[1]?.color === teams.colors[1]
+
+  return (
+    <>
+      <Box
+        className={css`
+          display: flex;
+          align-items: flex-end;
+          width: 100%;
+        `}
+      >
+        <DropdownTeamName type="A" dispatch={dispatch} />
+        <Box ml={1.5} />
+        <DropdownTeamName type="B" dispatch={dispatch} />
+      </Box>
+      <Box mt={3}>
+        <DropdownColors dispatch={dispatch} />
+      </Box>
+      <Box
+        mt={3}
+        className={css`
+          display: flex;
+          justify-content: space-between;
+          align-items: baseline;
+        `}
+      >
+        <Button
+          variant="outlined"
+          className={css`
+            white-space: nowrap;
+          `}
+          onClick={() => dispatch({ type: "swapColors" })}
+        >
+          Swap Colors
+        </Button>
+        <Box ml={1.5} />
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={replicateNameboard}
+          disabled={isUpdated()}
+        >
+          {isUpdated() ? "Updated" : "Update"}
+        </Button>
+      </Box>
+    </>
+  )
+}
+
 const DropdownColors = ({ dispatch }) => {
-  type Color = {
+  const dispatchColors = (event, newVal: ColorPair) => {
+    dispatch({
+      type: "setColors",
+      payload: [newVal[0].value, newVal[1].value],
+    })
+  }
+
+  type ColorPair = Array<{
     name: string
     value: string
-  }
-  type ColorPair = Array<Color>
+  }>
 
   const colorOptions: Array<ColorPair> = [
     [
@@ -266,6 +352,7 @@ const DropdownColors = ({ dispatch }) => {
   return (
     <Dropdown
       options={colorOptions}
+      onChange={dispatchColors}
       getOptionSelected={(o: ColorPair): boolean => colorOptions.includes(o)}
       getOptionLabel={(o: ColorPair) => `${o[0].name} vs ${o[1].name}`}
       renderOption={(o: ColorPair) => (
@@ -276,6 +363,34 @@ const DropdownColors = ({ dispatch }) => {
         </>
       )}
       name="Team Colors"
+    />
+  )
+}
+
+const DropdownTeamName = ({ type, dispatch }) => {
+  const dispatchTeamName = (event, newVal: any) => {
+    dispatch({
+      type: `setName${type}`,
+      payload: newVal,
+    })
+  }
+  const teams = [
+    "Xanadu",
+    "Persistence",
+    "Team Olive",
+    "Geek Squids",
+    "Twisted Time",
+    "Ink Eye",
+    "Red Sun",
+    "Assault and Pepper",
+  ]
+
+  return (
+    <Dropdown
+      options={teams}
+      onChange={dispatchTeamName}
+      getOptionSelected={(o): boolean => teams.includes(o)}
+      name={`Team Name ${type}`}
     />
   )
 }
@@ -292,11 +407,11 @@ const Dropdown = ({ name, options, ...rest }) => (
 )
 
 const InkPreview = ({ color }) => (
-  <Box>
+  <Box p={1.5}>
     <SvgIcon
       className={css`
         color: ${color};
-        font-size: ${theme.spacing(4)}px;
+        font-size: ${theme.spacing(5)}px;
         vertical-align: text-bottom;
       `}
     >
